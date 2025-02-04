f15fd9ca3b6b79383dccc06fb93d7d8b0a6ef921
Metal-Gear05 <jayson.sprouse25@compscihigh.org>
2025-02-04 22:00:29 +0000

    PA day 2

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
+[
+    {
+      "username": "NatureLover23",
+      "birdName": "Blue Jay",
+      "datePosted": 1707004800,
+      "birdLocation": "Central Park, NY",
+      "description": "Spotted this beautiful Blue Jay early in the morning. It was chirping loudly and hopping from branch to branch.",
+      "birdSpecies": "Cyanocitta cristata",
+      "forSale": false,
+      "marketPrice": 0,
+      "imageUrl": "https://example.com/images/bluejay.jpg"
+    },
+    {
+      "username": "FeatherFanatic",
+      "birdName": "Northern Cardinal",
+      "datePosted": 1706918400,
+      "birdLocation": "Atlanta Botanical Garden, GA",
+      "description": "A bright red male cardinal seen singing in the garden. Absolutely stunning!",
+      "birdSpecies": "Cardinalis cardinalis",
+      "forSale": false,
+      "marketPrice": 0,
+      "imageUrl": "https://example.com/images/northerncardinal.jpg"
+    },
+    {
+      "username": "WingWatcher",
+      "birdName": "African Grey Parrot",
+      "datePosted": 1706832000,
+      "birdLocation": "San Diego, CA",
+      "description": "A friendly and talkative parrot looking for a new home. Very intelligent and playful!",
+      "birdSpecies": "Psittacus erithacus",
+      "forSale": true,
+      "marketPrice": 1200,
+      "imageUrl": "https://example.com/images/africangrey.jpg"
+    },
+    {
+      "username": "SkyGazer",
+      "birdName": "Bald Eagle",
+      "datePosted": 1706745600,
+      "birdLocation": "Yellowstone National Park, WY",
+      "description": "Majestic bald eagle soaring over the river. A breathtaking sight to behold.",
+      "birdSpecies": "Haliaeetus leucocephalus",
+      "forSale": false,
+      "marketPrice": 0,
+      "imageUrl": "https://example.com/images/baldeagle.jpg"
+    },
+    {
+      "username": "ExoticBirdsOnly",
+      "birdName": "Macaw",
+      "datePosted": 1706659200,
+      "birdLocation": "Miami, FL",
+      "description": "Vibrant blue and yellow macaw available for adoption. Very social and loves to interact!",
+      "birdSpecies": "Ara ararauna",
+      "forSale": true,
+      "marketPrice": 2500,
+      "imageUrl": "https://example.com/images/macaw.jpg"
+    }
+  ]
 
+  
+  
